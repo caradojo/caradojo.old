2f406dbbc1dd36edc06a8add05fd4a7f6ef84213
Johan Martinsson <martinsson.johan@gmail.com>
2014-04-07 23:13:05 +0200

    when you click on submit you should go directly to the submission page without passing by propile home page

--- scripts/directives/orateursDirective.js
+++ scripts/directives/orateursDirective.js
@@ -6,7 +6,7 @@ AgileGrenobleApp.directive('agOrateurs', ['$window', function($window) {
 			replace: true,
 			controller: function ($scope) {
 				$scope.soumettre = function() {
-					$window.location.href = 'http://cfp.agile-grenoble.org/';
+					$window.location.href = 'http://cfp.agile-grenoble.org/sessions/new';
 				};
 			},
 			templateUrl: './views/orateurs.html'
