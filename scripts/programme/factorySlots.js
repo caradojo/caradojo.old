0c673ef6f18abafd6ac793684185d9d2f39fcf13
pierre.taillard <pierre_taillard@yahoo.fr>
2014-07-25 16:07:38 +0200

    change resource path

--- scripts/programme/factorySlots.js
+++ scripts/programme/factorySlots.js
@@ -1,4 +1,4 @@
 var AGProgrammeApp = AGProgrammeApp || {};
 AGProgrammeApp.factory('Slots', ['$resource', function($resource) {
-  	return $resource('/agile-grenoble-backend-master/resources/templates/slots.json');
+  	return $resource('slots.json');
 }]);
