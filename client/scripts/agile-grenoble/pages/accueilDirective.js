2739059664ffa24e1ff46ff06d7ade1e3ea165f5
pierre.taillard <pierre_taillard@yahoo.fr>
2015-11-11 16:54:02 +0100

    Fix: Sponsors are available again

--- client/scripts/agile-grenoble/pages/accueilDirective.js
+++ client/scripts/agile-grenoble/pages/accueilDirective.js
@@ -1,36 +1,36 @@
 var AgileGrenobleApp = AgileGrenobleApp || {};
 AgileGrenobleApp.directive('agAccueil', function() {
-		return {
-			restrict: 'A',
-			replace: true,
-			controller: function ($scope, $filter) {
-        var $translate = $filter('translate');
+    return {
+        restrict: 'A',
+        replace: true,
+        controller: function ($scope, $filter) {
+            var $translate = $filter('translate');
 
-        $scope.lienSoumission = function() {
-          return $translate('cfp.link.addr');
-		};
-		
-		$scope.lienRegister = function() {
-          return $translate('register.link.addr');		  		  
-        };
-		
-		$scope.lienListeDattente = function() {
-          return $translate('register.queueLink.addr');
-        };
+            $scope.lienSoumission = function() {
+            return $translate('cfp.link.addr');
+            };
 
-		$scope.lienAI2015 = function() {
-          return $translate('agileinnovation.link.addr');		  		  
-        };
+            $scope.lienRegister = function() {
+            return $translate('register.link.addr');
+            };
 
-		$scope.lienCaraContact = function() {
-          return $translate('cara.contactlink.addr');		  		  
-        };
+            $scope.lienListeDattente = function() {
+            return $translate('register.queueLink.addr');
+            };
 
-		$scope.lienCaraEvenements = function() {
-          return $translate('cara.eventslink.addr');		  		  
-        };
+            $scope.lienAI2015 = function() {
+            return $translate('agileinnovation.link.addr');
+            };
 
-	},
-	templateUrl: 'client/templates/agile-grenoble/accueil.html'
-	};
+            $scope.lienCaraContact = function() {
+            return $translate('cara.contactlink.addr');
+            };
+
+            $scope.lienCaraEvenements = function() {
+            return $translate('cara.eventslink.addr');
+            };
+
+        },
+        templateUrl: 'client/templates/agile-grenoble/accueil.html'
+    };
 });
