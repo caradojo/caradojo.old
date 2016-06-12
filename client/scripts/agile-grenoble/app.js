5b135c3f7d6a57ef7a04fb9c26f96f754f825795
Pierre Taillard <pierre_taillard@yahoo.fr>
2016-06-12 11:10:34 +0200

    Add notification for CFP + fix some visual issues on 'accueil' page

--- client/scripts/agile-grenoble/app.js
+++ client/scripts/agile-grenoble/app.js
@@ -1,4 +1,4 @@
-var AgileGrenobleApp = angular.module('AgileGrenobleApp', ['directives.skrollr', 'ngSanitize', 'angularSmoothscroll', 'ngRoute', 'ui.bootstrap', 'ui.utils', 'pascalprecht.translate', 'ngCookies'])
+var AgileGrenobleApp = angular.module('AgileGrenobleApp', ['ui-notification', 'directives.skrollr', 'ngSanitize', 'angularSmoothscroll', 'ngRoute', 'ui.bootstrap', 'ui.utils', 'pascalprecht.translate', 'ngCookies'])
 .config(['$translateProvider', function($translateProvider){
 
   $translateProvider.useStaticFilesLoader({
@@ -10,9 +10,24 @@ var AgileGrenobleApp = angular.module('AgileGrenobleApp', ['directives.skrollr',
 
   $translateProvider.useSanitizeValueStrategy('sanitize');
 }])
-.controller('AgileGrenobleCtrl', function($translate, $scope) {
-    $scope.setLang = function(langKey) {
+.config(function(NotificationProvider) {
+    NotificationProvider.setOptions({
+        delay: null,
+        startTop: 20,
+        startRight: 10,
+        verticalSpacing: 20,
+        horizontalSpacing: 20,
+        positionX: 'right',
+        positionY: 'bottom',
+        closeOnClick: true,
+        templateUrl: 'client/templates/agile-grenoble/notification.html'
+    });
+})
+.controller('AgileGrenobleCtrl', function($translate, $scope, Notification) {
+      $scope.setLang = function(langKey) {
         $translate.use(langKey);
       };
+      
+      Notification.primary({title: '<img src="client/media/icon_orange_speaker_100.png" width="40" height="40"/> Appel à orateurs'});
 });
 
