13e6de13094b54196183a275aa01c8dcb72dfe5d
Pierre Taillard <pierre_taillard@yahoo.fr>
2016-06-28 23:54:05 +0200

    Activate agile4kids

---
+++ client/scripts/agile4kids/pages/aproposDirective.js
@@ -0,0 +1,21 @@
+var AgileGrenobleApp = AgileGrenobleApp || {};
+AgileGrenobleApp.directive('agApropos', function () {
+  return {
+    restrict: 'A',
+    replace: true,
+    controller: function ($scope, $filter, ContentService) {
+      var $translate = $filter('translate');
+
+      $scope.content = ContentService.getCommonData().then(function (data) {
+        $scope.content = data;
+      }, function (data) {
+        $scope.content = data;
+      });
+
+      $scope.mails = function (strToTranslate) {
+        return $translate(strToTranslate);
+      };
+    },
+    templateUrl: 'client/templates/agile4kids/apropos.html'
+  };
+});
